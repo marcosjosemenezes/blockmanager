fa0009188176ccfee54470f70bf59cf2a33caff7
Marcos Santos <marcos.santos@onturis.com.br>
2019-09-21 22:26:00 -0300

    Second Commit

---
+++ integration/keygenarator.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @copyright Emprego Para Você Ltda
+ * @see Projeto http://blockmanager.com.br
+ * @package Integration key generator
+ * @author Marcos José Menezes dos Santos
+ * @since 2019-09-21 22:05
+ * @version 1.00
+ */
+
+if (!defined('INTEGRATION_KEY_GENERATOR')) { define('INTEGRATION_KEY_GENERATOR','asdak27js782hwq7asdh7u237asjdh-as8d72-0q878akhq73uwj'); }
+
+echo INTEGRATION_KEY_GENERATOR;
