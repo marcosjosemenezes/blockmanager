96bf859fce9001efa21a3926186f4129097d4d3c
Marcos J M Santos <marcosjosemenezes@bol.com.br>
2019-09-21 22:55:28 -0300

    Added Security File

---
+++ security/security.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @copyright Emprego Para Você Ltda
+ * @see Projeto http://blockmanager.com.br
+ * @package Security Project
+ * @author Marcos José Menezes dos Santos
+ * @since 2019-09-21 22:05
+ * @version 1.00
+ */
