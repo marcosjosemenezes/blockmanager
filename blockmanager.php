72e93adfeae1b5832b16ed5a627bf4905155b050
Marcos Santos <marcos.santos@onturis.com.br>
2019-09-21 22:15:45 -0300

    First Commit

---
+++ blockmanager.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @copyright Emprego Para Você Ltda
+ * @see Projeto http://blockmanager.com.br
+ * @package Start package for teste only
+ * @author Marcos José Menezes dos Santos
+ * @since 2019-09-21 22:05
+ * @version 1.00
+ */
+
+if (!defined('CMS_START_TEST_ONLY')) { define('CMS_START_TEST_ONLY','Created for test'); }
+
+echo CMS_START_TEST_ONLY;
